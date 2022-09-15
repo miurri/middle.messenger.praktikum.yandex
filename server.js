18844a3d6acaa1a8780a456e4f9f11336a51a455
Anna Lysenko <nlysenko76@ya.ru>
2022-09-16 02:29:51 +0300

    fix: Added http response for routing

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
-app.use(express.static('dist'));
+app.use(express.static(`${__dirname}/dist`));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
