03a642a3fe9e8a9748dafc57a586314450f1dd8c
Anna Lysenko <nlysenko76@ya.ru>
2022-09-29 21:27:43 +0300

    fix: fixes after review

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const DEFAULT_PORT = 3000;
+const port = process.env.PORT || DEFAULT_PORT;
 
 app.use(express.static(`${__dirname}/dist`));
 
