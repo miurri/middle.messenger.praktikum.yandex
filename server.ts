f3d5e0b50d7b1a1052c857f1fd80c29646f3a406
Anna Lysenko <nlysenko76@ya.ru>
2022-09-29 22:47:47 +0300

    chore: Added eslint, stylelint and typescript

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const DEFAULT_PORT = 3000;
@@ -7,5 +7,5 @@ const port = process.env.PORT || DEFAULT_PORT;
 app.use(express.static(`${__dirname}/dist`));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`);
 });
