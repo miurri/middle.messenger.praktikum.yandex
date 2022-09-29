e91db8248cbb244253c102c2fee2ee320f9be42f
Anna Lysenko <33584513+miurri@users.noreply.github.com>
2022-09-29 21:33:17 +0300

    Sprint 1 (#1)
    * feat: Created templates for all pages

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const DEFAULT_PORT = 3000;
+const port = process.env.PORT || DEFAULT_PORT;
+
+app.use(express.static(`${__dirname}/dist`));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
